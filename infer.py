d96b9053ad0f491f7ba79bf0d7a36d1793cff083
linstcl <linstcl@qq.com>
2022-07-28 14:07:23 +0800

    debug

--- infer.py
+++ infer.py
@@ -1,14 +1,11 @@
-from ast import arg
-from genericpath import isfile
-import cv2
+from pathlib import Path
 import paddle
-import paddle.nn as nn
+import paddle.nn
 from src.models import create_model
 from src.helper_functions.helper_functions import *
 import numpy as np
 import argparse
 import os
-from paddle.distributed import fleet
 
 parser = argparse.ArgumentParser(description='PyTorch TResNet ImageNet Inference')
 parser.add_argument('--params_path', type=str, default='params')
@@ -36,20 +33,30 @@ def main():
     # setup model
     print('creating model...')
     model = create_model(args)
+    params_path = Path(args.params_path)
     try:
-        temp = os.listdir(args.params_path)
-        if args.model_name not in temp:
+        temp = params_path.resolve()
+    except FileNotFoundError:
+        # 不存在
+        os.mkdir(args.params_path)
+    else:
+        # 存在
+        try:
+            temp = os.listdir(args.params_path)
+            if args.model_name not in temp:
+                print('creating now params...')
+                temp = os.getcwd()
+                os.chdir(args.params_path)
+                paddle.save(model.state_dict(), args.model_name+'.pdparams')
+                os.chdir(temp)
             temp = os.getcwd()
             os.chdir(args.params_path)
-            paddle.save(model.state_dict(), args.model_name+'.pdparams')
+            model_params = paddle.load(args.model_name+'.pdparams')
             os.chdir(temp)
-        temp = os.getcwd()
-        os.chdir(args.params_path)
-        model_params = paddle.load(args.model_name+'.pdparams')
-        os.chdir(temp)
-        model.set_state_dict(model_params)
-    except:
-        pass
+            model.set_state_dict(model_params)
+        except:
+            print('params load error')
+            return
     print('done\n')
 
     if args.train_mode == True:
